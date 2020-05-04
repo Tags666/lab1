e15d40524310cfa82410947fdcbdc0f897ee9ed6
Ilya <ilyas1991@inbox.ru>
2020-05-04 22:00:52 +0600

    intial commit

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.lab1;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -11,15 +14,16 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+
 import butterknife.BindView;
-import butterknife.ButterKnife;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
+    Button buttonClick;
     Fragment1 frag1;
     Fragment2 frag2;
     FragmentTransaction fTrans;
-
+    Button infoToast;
     @BindView(R.id.btnAdd)
     Button btnAdd;
     @BindView(R.id.btnRemove)
@@ -32,20 +36,37 @@ public class MainActivity extends Activity {
     FrameLayout frgmCont;
     @BindView(R.id.LinearLayout1)
     LinearLayout LinearLayout1;
+    @BindView(R.id.btnInfoToast)
+    Button btnToast;
 
+    Button button;
+
+    Button mCrowsCounterButton;
+    TextView mInfoTextView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
-        frag1 = new Fragment1();
+        Button buttonBack = (Button)findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(this);
+        buttonClick = (Button) findViewById(R.id.btnInfoToast);
+        infoToast = findViewById(R.id.btnInfoToast);
+        buttonClick = (Button) findViewById(R.id.btnInfoToast);
+        buttonClick.setOnClickListener(this);
+        infoToast.setOnClickListener(this);
+
+
+frag1 = new Fragment1();
         frag2 = new Fragment2();
 
         chbStack = (CheckBox) findViewById(R.id.chbStack);
+
+
     }
 
     public void onClick(View v) {
+
         fTrans = getFragmentManager().beginTransaction();
         switch (v.getId()) {
             case R.id.btnAdd:
@@ -58,16 +79,31 @@ public class MainActivity extends Activity {
                 fTrans.replace(R.id.frgmCont, frag2);
             default:
                 break;
+            case R.id.btnInfoToast:
+                Toast toast = Toast.makeText(getApplicationContext(),"Показать Toast",Toast.LENGTH_LONG);
+                toast.show();
+                break;
+            case R.id.buttonBack:
+                Intent i;
+
+                i = new Intent(this, BackActivity.class);
+
+                startActivity(i);
+
+
+
+                break;
+
+
+
         }
         if (chbStack.isChecked()) fTrans.addToBackStack(null);
         fTrans.commit();
-    }
-    public void showToast(View view) {
-        //создаём и отображаем текстовое уведомление
-        Toast toast = Toast.makeText(getApplicationContext(),
-                "Пора показать  Toast!",
-                Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER, 0, 0);
-        toast.show();
-    }
-}
+
+
+
+
+
+        }
+
+        }
